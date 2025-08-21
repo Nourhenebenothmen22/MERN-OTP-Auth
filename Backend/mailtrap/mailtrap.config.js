d17901f09e5c88fb0dff5f042aea87455710476b
nourhene <benothmennourhen8@gmail.com>
2025-08-21 19:19:56 +0100

    📧 Add Mailtrap integration with token from .env

---
+++ Backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,33 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+const ENDPOINT = process.env.MAILTRAP_ENDPOINT; // optionnel
+
+const client = new MailtrapClient({
+  token: TOKEN,
+   endpoint: ENDPOINT, 
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+
+const recipients = [
+  {
+    email: "benothmennourhen8@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log)
+  .catch(console.error);
