46d557317e7d1d9867c39245356b9fd7e255e7fe
nourhene <benothmennourhen8@gmail.com>
2025-08-21 18:51:54 +0100

    ✨ Implement signup with JWT, secure cookie & email verification

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,9 +1,45 @@
-export const signup=async (req,res)=>{
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // Vérifier si l'utilisateur existe déjà
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res
+        .status(400)
+        .json({ message: "User already exists with this email" });
+    }
+    // Hashage du mot de passe (12 salt rounds)
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    // Créer un nouvel utilisateur
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      verificationToken,
+      verificationTokenExpiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000), // expire dans 24h
+    });
+   generateTokenAndSetCookie(res, newUser._id);
 
-}
-export const login=async (req,res)=>{
-    
-}
-export const logout=async (req,res)=>{
-    
-}
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+export const login = async (req, res) => {};
+export const logout = async (req, res) => {};
